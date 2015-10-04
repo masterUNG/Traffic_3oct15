f21a001643a746018436c86c846bb411a3cc3650
masterUNG <phrombutr@gmail.com>
2015-10-04 12:52:17 +0700

    Start Day2

---
+++ app/src/main/java/appewtc/masterung/traffic/MainActivity.java
@@ -0,0 +1,111 @@
+package appewtc.masterung.traffic;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+        //Create ListView
+        createListView();
+
+
+    }   // Main Method
+
+    private void createListView() {
+
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลียวซ้าย";
+        strTitle[1] = "ห้ามเลียวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลียวขวา";
+        strTitle[4] = "เลียวซ้าย";
+        strTitle[5] = "ออก";
+        strTitle[6] = "เข้า";
+        strTitle[7] = "ออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "จำกัดความสูง";
+        strTitle[10] = "แยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอง";
+        strTitle[13] = "สวนทาง";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "เข้า";
+        strTitle[16] = "หยุดตรวม";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำัดความกว้าง";
+        strTitle[19] = "จำกัดความสูง";
+
+
+        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intIcon, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
+
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }   // event
+        });
+
+
+
+    }   // createListView
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                buttonMediaPlayer.start();
+
+                //Intent To WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/VTStiGuXRjE"));
+                startActivity(objIntent);
+
+            }   // event
+        });
+
+    }   // buttonController
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.livTraffic);
+        aboutMeButton = (Button) findViewById(R.id.btnAboutMe);
+
+    }   // bindWidget
+
+}   // Main Class นี่คือ คลาสหลัก เว้ยเห้ย
---
+++ app/src/main/java/appewtc/masterung/traffic/MyAdapter.java
@@ -0,0 +1,60 @@
+package appewtc.masterung.traffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by masterUNG on 10/4/15 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.traffic_listview, viewGroup, false);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
+
+        //Setup Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.txtShowTitle);
+        titleTextView.setText(titleStrings[i]);
+
+        return objView1;
+    }   // getView
+
+
+}   // Main Class
